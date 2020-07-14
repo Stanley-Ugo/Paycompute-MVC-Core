04eb55e7a24cae2734b877ab2d238f5af02f998f
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-15 00:05:10 +0100

    Added Properties to IndexViewModel

---
+++ Paycompute/Controllers/PayController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Controllers
+{
+    public class PayController : Controller
+    {
+        private readonly IPayComputationService _payComputationService;
+
+        public PayController(IPayComputationService payComputationService)
+        {
+            _payComputationService = payComputationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Paycompute/Models/PaymentRecordIndexViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Models
+{
+    public class PaymentRecordIndexViewModel
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee employee { get; set; }
+
+        [Display(Name = "Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Pay Date")]
+        public DateTime PayDate { get; set; }
+
+        [Display(Name = "Month")]
+        public string PayMonth { get; set; }
+
+        [Display(Name = "Total Earnings")]
+        public int TaxYearId { get; set; }
+
+        [Display(Name = "Tatal Deductions")]
+        public string Year { get; set; }
+        public decimal TotalEarnings { get; set; }
+
+        [Display(Name = "Total Deductions")]
+        public decimal TotalDeduction { get; set; }
+
+        [Display(Name = "Net")]
+        public decimal NetPayment { get; set; }
+    }
+}
