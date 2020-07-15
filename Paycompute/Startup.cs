2b059d69efc9a9c2c3fddc97c40f7b807bf47450
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-15 22:33:47 +0100

    Register Services and added Index view - PayController

--- Paycompute/Startup.cs
+++ Paycompute/Startup.cs
@@ -38,6 +38,9 @@ namespace Paycompute
             services.AddControllersWithViews();
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddRazorPages();
+            services.AddScoped<IPayComputationService, PayComputationService>();
+            services.AddScoped<INationalInsuranceContributionService, NationalInsuranceContributionService>();
+            services.AddScoped<ITaxService, TaxService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
